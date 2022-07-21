4dcf4b6e209e9b7238e9456dd363a3242334c6e7
Harshit Sharma <harshit19csu411@ncuindia.edu>
2022-07-22 00:11:22 +0530

    enabled cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const { json } = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use("/", authRouter)
